375d5c13e1169766aa6208c54e4cac061da66bcf
Karly <53051292+KarlyKizewski@users.noreply.github.com>
2019-10-08 19:40:46 -0400

    Update tryagain.rb

--- tryagain.rb
+++ tryagain.rb
@@ -33,3 +33,18 @@ end
 
 deck = Deck.new
 deck.deal 
+
+
+# Prototypical Inheritance in JavaScript
+function Card(rank, suit) {
+  this.rank = rank;
+  this.suit = suit;
+}
+
+Card.prototype.outputCard = function(){
+  console.log(this.rank + " of " + this.suit);
+}
+
+
+var fourOfDiamonds = new Card(4, "diamonds");
+fourOfDiamonds.outputCard();
