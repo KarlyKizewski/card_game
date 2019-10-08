2043489d39183809afc91589c756814df4035f86
Karly <53051292+KarlyKizewski@users.noreply.github.com>
2019-10-08 19:08:52 -0400

    Add files via upload

---
+++ tryagain.rb
@@ -0,0 +1,35 @@
+class Card
+  attr_accessor :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def output_card
+    puts "#{self.rank} of #{self.suit}"
+  end
+end
+
+class Deck
+  def initialize
+    @cards = []
+    @ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K, :A]
+    @suits = [:hearts, :spades, :diamonds, :clubs]
+
+    @ranks.each do |rank|
+      @suits.each do |suit|
+        @cards << Card.new(rank,suit)
+      end
+    end
+
+  @cards.shuffle!
+  end
+
+  def deal
+    @cards.shift.output_card
+  end
+end
+
+deck = Deck.new
+deck.deal 
